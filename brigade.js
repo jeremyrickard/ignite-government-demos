051e21532870aa65226be09bc1d9029e19d17ea6
Jeremy Rickard <jeremy.rickard@microsoft.com>
2019-01-31 21:38:50 -0700

    added java test to brig

--- brigade.js
+++ brigade.js
@@ -2,5 +2,21 @@ const {events, Job} = require("brigadier");
 
 events.on("exec", (e, project) => {
   console.log("exec hook fired, manual test");
+
+  var job = createJavaTest(e, project)
+  job.run()
 });
+
+function createJavaTest(e, project) {
+  var javaTestJob = new Job("java-tests")
+
+  javaTestJob.image= "maven:3-jdk-11"
+
+  javaTestJob.tasks = [
+    "cd src/draft-boot",
+    "mvn test"
+  ];
+
+  return javaTestJob
+}
 
