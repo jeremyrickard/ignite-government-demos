540a768c959386e1311e7e91a853a068d54f2747
Jeremy Rickard <jeremy.rickard@microsoft.com>
2019-01-31 21:13:51 -0700

    brig script run tests

--- draft-boot-brig/brigade.js
+++ draft-boot-brig/brigade.js
@@ -2,4 +2,20 @@ const {events, Job} = require("brigadier");
 
 events.on("exec", (e, project) => {
   console.log("exec hook fired, manual test");
+  runTests(e, project);
 });
+
+function javaTests(e, project) {
+  var javaTests = new Job("java tests", "maven:3-jdk-11")
+ 
+  javaTests.tasks = [
+    "cd src/draft-boot",
+    "mvn test"
+  ];
+
+  return javaTests
+}
+
+function runTests(e, project) {
+  javaTest(e, project).catch(e  => {console.error(e.toString())});
+}
